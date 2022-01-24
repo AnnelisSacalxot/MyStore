ad745ff35fa0f29a66c56a2f21d911843fab41b1
Annelís <juanysacalxot376@gmail.com>
2022-01-24 11:49:48 -0600

    Creacion de nuevo modulo creado con el CLI: products

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {ContainerStoreComponent} from "./pages/containerStore/containerStore.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'containerStore', component: ContainerStoreComponent},
+  { path: 'products', loadChildren: () => import('./pages/products/products.module').then(m => m.ProductsModule) },
+  //siempre se pone de último porque sino no puede evaluar las demas rutas
+  //y no genere ningun problema
+  {path: '**', redirectTo: '', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
